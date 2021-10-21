8e4f84eaea47ebca9623a415acf58c06c73398b9
Ezequiel <ezequielamin@outlook.com>
2021-10-21 09:34:07 -0300

    Primer commit

---
+++ src/algoritmos/Algoritmos.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algoritmos;
+
+/**
+ *
+ * @author EZEA2
+ */
+/*public class Algoritmos {
+    static Logic lo;
+    /**
+     * @param args the command line arguments
+     
+    public static void main(String[] args) {
+        lo=new Logic(true);
+        
+        
+        // TODO code application logic here
+        
+        /*
+        ConectarBaseDeDatos con=new ConectarBaseDeDatos();
+        con.conectar();
+    }
+    
+    
+    
+}*/
+
+public class Algoritmos {
+    /*private static GUICantidad ventanaIngreso;
+    private static GUIGenerar ventanaGenerar;
+    private static int n;
+    /**
+     * @param args the command line arguments
+     
+    
+    Algoritmos(){}
+    
+    public static void main(String[] args) {
+        ventanaIngreso = new GUICantidad();
+        ventanaGenerar = new GUIGenerar();
+        ventanaIngreso.setVisible(true);
+        ventanaIngreso.setLocationRelativeTo(null);
+        
+        
+        // TODO code application logic here
+        
+        /*
+        ConectarBaseDeDatos con=new ConectarBaseDeDatos();
+        con.conectar();
+    }
+    
+    public void finIngreso(int _n){
+        n=_n;
+        
+        ventanaIngreso.setVisible(false);
+        
+        ventanaGenerar .setVisible(true);
+        ventanaGenerar .setLocationRelativeTo(null);
+    }*/
+    
+    
+    
+}
